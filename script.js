0597eb8218fb686b4a22de797d9a6ee6a2249022
rlwinchester <Robert Winchester@RW-Surface3.comptia.org>
2015-04-22 08:00:18 -0500

    Upload full

---
+++ script.js
@@ -0,0 +1,64 @@
+$(document).ready(function() {
+    $('#USBtext').draggable({
+        opacity:.5,
+        create: function(){$(this).data('position',$(this).position())},
+        cursorAt:{left:15},
+        cursor:'move',
+        revert: 'invalid',
+        start:function(){$(this).stop(true,true)}
+		});
+		
+	$('#DVItext').draggable({
+        opacity:.5,
+        create: function(){$(this).data('position',$(this).position())},
+        cursorAt:{left:15},
+        cursor:'move',
+        revert: 'invalid',
+        start:function(){$(this).stop(true,true)}
+    });
+
+    $("#USBtext").data({'originalLeft': $("#USBtext").css('left'),
+        'origionalTop': $("#USBtext").css('top')
+    });
+	 $("#DVItext").data({'originalLeft': $("#DVItext").css('left'),
+        'origionalTop': $("#DVItext").css('top')
+    });
+
+    $('#USBimg').droppable({
+        drop: function( event, ui ) {
+            $(this).droppable('option', 'accept', ui.draggable);
+    
+  },
+  out: function( event, ui ) {
+             $(this).droppable('option', 'accept');
+ //           $(this).removeClass( "dropped-highlight" );
+  }
+});
+    $('#DVIimg').droppable({
+        drop: function( event, ui ) {
+            $(this).droppable('option', 'accept', ui.draggable);
+        },
+  out: function( event, ui ) {
+      $(this).droppable('option', 'accept');
+      //           $(this).removeClass( "dropped-highlight" );
+  }
+});
+
+    $(".reset").click(function () {
+        $("#USBtext").css({
+            'left': $("#USBtext").data('originalLeft'),
+            'top': $("#USBtext").data('origionalTop')
+        })
+		  $("#DVItext").css({
+            'left': $("#DVItext").data('originalLeft'),
+            'top': $("#DVItext").data('origionalTop')
+        })
+    });
+
+
+    });
+    function snapToMiddle(dragger, target){
+        var topMove = target.position().top - dragger.data('position').top + (target.outerHeight(true) - dragger.outerHeight(true)) / 1;
+        var leftMove= target.position().left - dragger.data('position').left + (target.outerWidth(true) - dragger.outerWidth(true)) / 2;
+        dragger.animate({top:topMove,left:leftMove},{duration:600,easing:'easeOutBack'});
+    }
