e1a111656de463814aa0996d7b30c0feee2095ad
rlwinchester <Robert Winchester@RW-Surface3.comptia.org>
2015-04-22 16:00:01 -0500

    Cleaned up

--- js/script.js
+++ js/script.js
@@ -1,76 +1,34 @@
-$(document).ready(function() {
-    $('#USBtext').draggable({
-        opacity:.5,
-        create: function(){$(this).data('position',$(this).position())},
-        cursorAt:{left:15},
-        cursor:'move',
+$(document).ready(function () {
+    $('#USBtext,#DVItext,#FireWiretext,#HDMItext,#VGAtext,#Fibertext').draggable({
+        opacity: .5,
+        create: function () { $(this).data('position', $(this).position()) },
+        cursorAt: { left: 15 },
+        cursor: 'move',
         revert: 'invalid',
-        start:function(){$(this).stop(true,true)}
-		});
-		
-	$('#DVItext').draggable({
-        opacity:.5,
-        create: function(){$(this).data('position',$(this).position())},
-        cursorAt:{left:15},
-        cursor:'move',
-        revert: 'invalid',
-        start:function(){$(this).stop(true,true)}
-	});
-	$('#FireWiretext').draggable({
-	    opacity: .5,
-	    create: function () { $(this).data('position', $(this).position()) },
-	    cursorAt: { left: 15 },
-	    cursor: 'move',
-	    revert: 'invalid',
-	    start: function () { $(this).stop(true, true) }
-	});
-
-    $("#USBtext").data({'originalLeft': $("#USBtext").css('left'),
-        'origionalTop': $("#USBtext").css('top')
+        start: function () { $(this).stop(true, true) }
     });
-	 $("#DVItext").data({'originalLeft': $("#DVItext").css('left'),
-        'origionalTop': $("#DVItext").css('top')
-	 });
-	 $("#FireWiretext").data({
-	     'originalLeft': $("#FireWiretext").css('left'),
-	     'origionalTop': $("#FireWiretext").css('top')
-	 });
+    $('#USBtext,#DVItext,#FireWiretext,#HDMItext,#VGAtext,#Fibertext').data({
+        'originalLeft': $('#USBtext,#DVItext,#FireWiretext,#HDMItext,#VGAtext,#Fibertext').css('left'),
+        'origionalTop': $('#USBtext,#DVItext,#FireWiretext,#HDMItext,#VGAtext,#Fibertext').css('top')
+    });
 
-    $('#USBimg').droppable({
-        drop: function( event, ui ) {
-            $(this).droppable('option', 'accept', ui.draggable);
-    
-  },
-  out: function( event, ui ) {
-             $(this).droppable('option', 'accept');
- //           $(this).removeClass( "dropped-highlight" );
-  }
-});
-    $('#DVIimg').droppable({
-        drop: function( event, ui ) {
-            $(this).droppable('option', 'accept', ui.draggable);
-        },
-  out: function( event, ui ) {
-      $(this).droppable('option', 'accept');
-      //           $(this).removeClass( "dropped-highlight" );
-  }
-});
+    $('#USBimg,#DVIimg,#FireWireimg,#HDMIimg,#VGAimg,#Fiberimg').droppable({
+        drop: function (event, ui) {
+            snapToMiddle(ui.draggable, $(this));
+        }
+    });
 
     $(".reset").click(function () {
-        $("#USBtext").css({
-            'left': $("#USBtext").data('originalLeft'),
-            'top': $("#USBtext").data('origionalTop')
-        })
-		  $("#DVItext").css({
-            'left': $("#DVItext").data('originalLeft'),
-            'top': $("#DVItext").data('origionalTop')
+        $('#USBtext,#DVItext,#FireWiretext,#HDMItext,#VGAtext,#Fibertext').css({
+            'left': $('#USBtext,#DVItext,#FireWiretext,#HDMItext,#VGAtext,#Fibertext').data('originalLeft'),
+            'top': $('#USBtext,#DVItext,#FireWiretext,#HDMItext,#VGAtext,#Fibertext').data('origionalTop')
         })
     });
 
 
-    });
-    function snapToMiddle(dragger, target){
-        var topMove = target.position().top - dragger.data('position').top + (target.outerHeight(true) - dragger.outerHeight(true)) / 1;
-        var leftMove= target.position().left - dragger.data('position').left + (target.outerWidth(true) - dragger.outerWidth(true)) / 2;
-        dragger.animate({top:topMove,left:leftMove},{duration:600,easing:'easeOutBack'});
-    }
+});
+function snapToMiddle(dragger, target) {
+    var topMove = target.position().top - dragger.data('position').top + (target.outerHeight(true) - dragger.outerHeight(true)) / 2;
+    var leftMove = target.position().left - dragger.data('position').left + (target.outerWidth(true) - dragger.outerWidth(true)) / 2;
+    dragger.animate({ top: topMove, left: leftMove }, { duration: 600, easing: 'easeOutBack' });
+}
